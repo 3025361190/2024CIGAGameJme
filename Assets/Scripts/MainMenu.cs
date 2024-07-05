7537b6bea6bc85c5736c9cda870610c557d3dea1
methiy <3250949283@qq.com>
2024-07-05 22:03:35 +0800

    MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+
+    }
+
+    public void QuitGame(){
+
+    }
+
+    public void GotoMainMenu(){
+
+    }
+
+    public void GotoSettingMenu(){
+
+    }
+}
