b4e37fbb54add34c966420a860e35b5eb8e22172
FortunateRui <68178029+FortunateRui@users.noreply.github.com>
2024-07-07 06:57:31 +0800

    回滚半夜写的bug

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,13 +1,6 @@
-/*
-文件名：Manager.cs
-编辑人：豪哥，fortunate瑞，没道理啊
-文件描述：Manager类，用于管理游戏的场景切换，ui界面，游戏流程等
-*/
-
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Threading;
 using UnityEngine;
 
 
@@ -15,49 +8,18 @@ using UnityEngine;
 public class Manager : MonoBehaviour
 {
     public GameObject turret;
-    private SceneType currentSceneType;
-    private bool cdFlag = false;
-    private float cdTime = 5.0f;
-    private float cdTimer = 0.0f;
+    private SceneType currentSceneType = SceneType.QingTang;
 
-    public Sprite[] sceneResource;
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentSceneType = SceneType.QingTang;
-        GetComponent<SpriteRenderer>().sprite = sceneResource[1];
-        // currentSceneType = SceneType.HongYou;
-        // GetComponent<SpriteRenderer>().sprite = sceneResource[0];
-        turret =  GameObject.FindGameObjectsWithTag("Turret")[0];
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(cdFlag){
-            cdTimer += Time.deltaTime;
-            if(cdTimer >= cdTime){
-                cdFlag = false;
-                cdTimer = 0.0f;
-            }
-        }
-    }
     public SceneType GetCurrentSceneType(){
         return currentSceneType;
     }
 
-    public void SwitchSceneType(){
+    private void SwitchSceneType(){
         if(currentSceneType == SceneType.QingTang){
             currentSceneType = SceneType.HongYou;
             Recovery();
-            cdFlag = true;
-            // 切换美术资源
-            // GetComponent<SpriteRenderer>().sprite = sceneResource[0];
-        }
-        else if(cdFlag == false && currentSceneType == SceneType.HongYou)
-        {
+        }else{
             currentSceneType = SceneType.QingTang;
-            // GetComponent<SpriteRenderer>().sprite = sceneResource[1];
         }
     }
 
@@ -65,30 +27,15 @@ public class Manager : MonoBehaviour
         turret.GetComponent<ShootBullet>().RecycleBullet();
     }
 
-    // 游戏暂停
-    public void PauseGame(){
-        Time.timeScale = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
 
-    // 游戏继续
-    public void ResumeGame(){
-        Time.timeScale = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
-
-    // 返回主菜单
-    public void BackToMenu(){
-        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
-    }
-
-    // 开始游戏
-    public void StartGame(){
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
-    }
-
-    // 重新开始游戏
-    public void RestartGame(){
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
-    }
-
-    
 }
