a31b3f62f704dae9e919f6fd0c88d85f6fda45f0
3025361190 <119148001+3025361190@users.noreply.github.com>
2024-07-06 10:31:02 +0800

    commen

---
+++ Assets/Scripts/commen.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class commen : MonoBehaviour
+{
+    public enum CollerType
+    {
+        Red,
+        Yellow,
+        Blue,
+        Green,
+        Black,
+        White,
+        Purple
+    }
+}
