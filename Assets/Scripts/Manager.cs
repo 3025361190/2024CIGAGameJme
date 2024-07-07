17f2d6d1bbef93f8cdd1d854b8fe2eb35382dfb3
FortunateRui <68178029+FortunateRui@users.noreply.github.com>
2024-07-07 13:42:07 +0800

    1

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -67,7 +67,7 @@ public class Manager : MonoBehaviour
             cdFlag = true;
             // 切换美术资源
             // GetComponent<SpriteRenderer>().sprite = sceneResource[0];
-            //清汤切红油动画
+            // 清汤切红油动画
             beijing1.SetBool("background",true);
             beijing2.SetTrigger("change");
 
@@ -77,8 +77,6 @@ public class Manager : MonoBehaviour
             if(cdFlag == false){
                 Debug.Log("switch scene to 清汤");
                 currentSceneType = SceneType.QingTang;
-                Recovery();
-                cdFlag = true;
                 // GetComponent<SpriteRenderer>().sprite = sceneResource[1];
                 //红油切清汤动画
                 beijing1.SetBool("background", false);
@@ -90,15 +88,6 @@ public class Manager : MonoBehaviour
             
             }
         }
-        // else if(cdFlag == false && currentSceneType == SceneType.HongYou)
-        // {
-        //     Debug.Log("switch scene to 清汤");
-        //     currentSceneType = SceneType.QingTang;
-        //     // GetComponent<SpriteRenderer>().sprite = sceneResource[1];
-        //     //红油切清汤动画
-        //     beijing1.SetBool("background", false);
-        //     beijing2.SetTrigger("change");
-        // }
     }
 
     private void Recovery(){
