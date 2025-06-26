a6f5e85945e48c67eaefa49b79c1b6a1ac24a604
FortunateRui <68178029+FortunateRui@users.noreply.github.com>
2025-06-26 17:25:02 +0800

    add buffmanager

---
+++ Assets/Scripts/Buffs/BuffManager.cs
@@ -0,0 +1,37 @@
+/*
+文件名：BuffManager.cs
+编辑人：fortunate瑞
+文件描述：用于管理buff，暂定继承MonoBehaviour
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffManager : MonoBehaviour
+{
+
+    // 构造函数
+    public BuffManager()
+    {
+        Init();
+    }
+    // 游戏restart或者初次初始化时调用
+    void Init()
+    {
+    
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
