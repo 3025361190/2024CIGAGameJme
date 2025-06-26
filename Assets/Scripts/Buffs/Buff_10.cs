876492cf1fdac223fd389cc3bb0cac70fedce517
3025361190 <119148001+3025361190@users.noreply.github.com>
2025-06-27 02:55:04 +0800

    再copy几个

---
+++ Assets/Scripts/Buffs/Buff_10.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+文件名：Buff_10.cs
+编辑人：没道理啊
+文件描述：更加清汤buff
+*/
+public class Buff_10 : BaseBuff
+{
+    private float value;
+    private float originalsplitModeDuration;
+
+    public override void Init()
+    {
+        // 初始化buff参数
+        buffId = 7;
+        var buffConfig = GameManager.Instance.data.buffData[buffId];
+        buffName = buffConfig["buffName"].ToString();
+        buffDescription = buffConfig["buffDescription"].ToString();
+        buffIcon = buffConfig["buffIcon"].ToString();
+        buffDuration = buffConfig["buffDuration"].ToObject<float>();
+        buffStackable = buffConfig["buffStackable"].ToObject<bool>();
+        stackType = buffConfig["stackType"].ToObject<int>();
+        isNormalBuff = buffConfig["isNormalBuff"].ToObject<bool>();
+        value = buffConfig["value"].ToObject<float>();
+        originalsplitModeDuration = GameManager.Instance.data.modeData["splitModeDuration"].ToObject<float>();
+        // 初始化运行时参数
+        currentStack = 0;
+        isActive = false;
+        target = null;
+    }
+
+    public override void ActivateBuff()
+    {
+        // 保留父类逻辑
+        base.ActivateBuff();
+        if(stackType == 0)
+        {
+            GameManager.Instance.data.modeData["splitModeDuration"] = GameManager.Instance.data.modeData["splitModeDuration"].ToObject<float>() + value;
+        }
+        else if(stackType == 1)
+        {
+            GameManager.Instance.data.modeData["splitModeDuration"] = GameManager.Instance.data.modeData["splitModeDuration"].ToObject<float>() * value;
+        }
+    }
+
+    public override void DeactivateBuff()
+    {
+        base.DeactivateBuff();
+        GameManager.Instance.data.modeData["splitModeDuration"] = originalsplitModeDuration;
+    }    
+}
---
+++ Assets/Scripts/Buffs/Buff_8.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+文件名：Buff_8.cs
+编辑人：没道理啊
+文件描述：强劲体力buff
+*/
+
+//TODO: data里要不要存储一些游戏的中间值，比如子弹当前数量、人物当前血量......？？？
+//这个buff是不是应该增加最大血量以及人物当前血量？？？
+public class Buff_8 : BaseBuff
+{
+    public override void Init()
+    {
+
+    }
+}
