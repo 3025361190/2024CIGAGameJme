f0be7c6a26d7a6ed8e0fdf9951d6048a97445cad
FortunateRui <68178029+FortunateRui@users.noreply.github.com>
2025-06-27 23:32:59 +0800

    buff导入和显示初步完成，buff生效逻辑还未实现

--- Assets/Scripts/Buffs/BuffManager.cs
+++ Assets/Scripts/Buffs/BuffManager.cs
@@ -8,6 +8,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;  // 添加UI组件的命名空间
 
 public class BuffManager : MonoBehaviour
 {
@@ -29,6 +30,9 @@ public class BuffManager : MonoBehaviour
         }
     }
 
+    // buff选择界面
+    private GameObject buffChoose;
+
     // 用来存储绑定在当前GameObject上的buff（即已经实现的buff，buffid作为key）
     private Dictionary<int, BaseBuff> buffs = new();
 
@@ -40,7 +44,7 @@ public class BuffManager : MonoBehaviour
     // 一个list存储当前可以选择的boss buff id
     private List<int> bossBuffIds = new();
 
-    // 一个list存储当前已经激活的buffid
+    // 一个list存储当前已经激活且持续的buffid
     private List<int> activeBuffIds = new();
 
     // 单例
@@ -74,11 +78,21 @@ public class BuffManager : MonoBehaviour
         Component[] allComponents = GetComponents<Component>();
         foreach (var comp in allComponents)
         {
-            // 如果component是BaseBuff的子类，则将buff添加到buffs字典中
+            // 如果component是BaseBuff的子类
             if (comp is BaseBuff buff)
             {
                 buff.Init();
+                // 将buff添加到buffs字典中
                 buffs[buff.buffId] = buff;
+                // 根据buff类型添加到normalBuffIds或bossBuffIds
+                if (buff.isNormalBuff)
+                {
+                    normalBuffIds.Add(buff.buffId);
+                }
+                else
+                {
+                    bossBuffIds.Add(buff.buffId);
+                }
             }
         }
     }
@@ -104,13 +118,51 @@ public class BuffManager : MonoBehaviour
     public void ShowBuffChoose(bool isBossLevel)
     {
         Debug.Log("展示buff选择界面");
+        buffChoose = GameObject.Find("buffChoose");
         // 处理buff选择界面
-        GameObject buffChoose = GameObject.Find("buffChoose");
         if (buffChoose != null)
         {
             // 设置buff选项
-            // TODO: 根据isBossLevel设置buff选项
+            if (isBossLevel)
+            {
+                List<int> bossBuffIndexs = new();
+                List<int> normalBuffIndexs = new();
+                // 如果可选的boss buff数量不足3个，则用普通buff补充
+                int max = bossBuffIds.Count;
+                if(max >= 3)
+                {
+                    bossBuffIndexs = RandomUtils.GetUniqueRandomIntegers(0, max, 3);
+                    SetBuffCard(0,buffs[bossBuffIds[bossBuffIndexs[0]]]);
+                    SetBuffCard(1,buffs[bossBuffIds[bossBuffIndexs[1]]]);
+                    SetBuffCard(2,buffs[bossBuffIds[bossBuffIndexs[2]]]);
+                }
+                else
+                {
+                    for(int i = 0; i < max; i++)
+                    {
+                        SetBuffCard(i, buffs[bossBuffIds[i]]);
+                    }
+                    int n = 3-max;
+                    normalBuffIndexs = RandomUtils.GetUniqueRandomIntegers(0, normalBuffIds.Count, n);
+                    for(int i = 0; i < n; i++)
+                    {
+                        SetBuffCard(i+max, buffs[normalBuffIds[normalBuffIndexs[i]]]);
+                    }
+                }
 
+                
+            }
+            else
+            {
+                List<int> normalBuffIndexs = new();
+                int max = normalBuffIds.Count;
+                normalBuffIndexs = RandomUtils.GetUniqueRandomIntegers(0, max, 3);
+                for(int i = 0; i < 3; i++)
+                {
+                    SetBuffCard(i, buffs[normalBuffIds[normalBuffIndexs[i]]]);
+                }
+            }
+            
             // 递归设置所有层级的Animator组件
             SetAnimatorUnscaledTimeRecursively(buffChoose.transform);
 
@@ -123,6 +175,74 @@ public class BuffManager : MonoBehaviour
         else
         {
             Debug.LogError("未找到名为buffChoose的预制体");
+        }
+    }
+
+    // 设置buff卡面内容
+    private void SetBuffCard(int index, BaseBuff buff)
+    {
+        // 获取对应的buff卡片（buff1/2/3）
+        Transform buffCard = buffChoose.transform.Find($"buffChoose/layout/buff{index + 1}");
+        if (buffCard != null)
+        {
+            // 设置图标
+            Transform icon = buffCard.Find("icon");
+            if (icon != null)
+            {
+                // 设置图标
+                var iconImage = icon.GetComponent<Image>();
+                if (iconImage != null)
+                {
+                    // 如果有自定义图标则加载，否则保持默认图标
+                    if (!string.IsNullOrEmpty(buff.buffIcon))
+                    {
+                        Sprite iconSprite = Resources.Load<Sprite>($"BuffIcon/{buff.buffIcon}");
+                        if (iconSprite != null)
+                        {
+                            iconImage.sprite = iconSprite;
+                            Debug.Log($"buff{index+1}卡片的icon设置为{buff.buffIcon}");
+                        }
+                        else
+                        {
+                            Debug.LogError($"iconSprite不正确，buffIcon:{buff.buffIcon}，使用了占位图标");
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.LogError($"未找到buff{index+1}卡片icon中的Image组件");
+                }
+            }
+            else
+            {
+                Debug.LogError($"未找到buff{index+1}卡片的icon");
+            }
+
+            // 设置标题
+            Transform describeTitle = buffCard.Find("describeTitle");
+            if (describeTitle != null)
+            {
+                describeTitle.GetComponent<Text>().text = buff.buffName;
+            }
+            else
+            {
+                Debug.LogError($"未找到buff{index+1}卡片的describeTitle");
+            }
+
+            // 设置描述
+            Transform describe = buffCard.Find("describe");
+            if (describe != null)
+            {
+                describe.GetComponent<Text>().text = buff.buffDescription;
+            }
+            else
+            {
+                Debug.LogError($"未找到buff{index+1}卡片的describe");
+            }
+        }
+        else
+        {
+            Debug.LogError($"未找到buff{index+1}卡片");
         }
     }
 
