975e77440a0c21e84a41c5cf865bc1fcac1abadd
3025361190 <119148001+3025361190@users.noreply.github.com>
2025-07-01 22:55:58 +0800

    boss血条跟随

--- Assets/Scripts/EnemyScripts/BossHealth.cs
+++ Assets/Scripts/EnemyScripts/BossHealth.cs
@@ -220,3 +220,5 @@ public class BossHealth : MonoBehaviour
 
 
 // TODO: 血条跟随boss移动无法实现，现在和player血条在同一位置，详情查看boss预制体
+// TODO：re:因为没有独立的Canvas，所以血条无法跟随boss移动，
+// 需要在boss预制体中添加一个Canvas，并将血条放在Canvas中，
