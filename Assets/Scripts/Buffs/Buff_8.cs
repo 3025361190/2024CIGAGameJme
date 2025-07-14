13c51dadfbc01435d36c7af7613b2be8e421f6fd
FortunateRui <68178029+FortunateRui@users.noreply.github.com>
2025-07-14 23:39:38 +0800

    buff8测试完成

--- Assets/Scripts/Buffs/Buff_8.cs
+++ Assets/Scripts/Buffs/Buff_8.cs
@@ -21,7 +21,8 @@ public class Buff_8 : BaseBuff
         // 初始化buff参数
         buffId = 8;
         // 在buffs数组中查找对应buffId的配置
-        var buffsArray = GameManager.Instance.data.globalData["initialHealth"] as JArray;
+        // 在buffs数组中查找对应buffId的配置
+        var buffsArray = GameManager.Instance.data.buffData["buffs"] as JArray;
         var buffConfig = buffsArray.FirstOrDefault(b => b["buffId"].ToObject<int>() == buffId);
         buffName = buffConfig["buffName"].ToString();
         buffDescription = buffConfig["buffDescription"].ToString();
@@ -32,7 +33,7 @@ public class Buff_8 : BaseBuff
         isNormalBuff = buffConfig["isNormalBuff"].ToObject<bool>();
         value = buffConfig["value"].ToObject<int>();
         originalinitialHealth = GameManager.Instance.data.globalData["initialHealth"].ToObject<int>();
-        originalcurrentHealth = GameManager.Instance.currentHealth;
+        // originalcurrentHealth = GameManager.Instance.currentHealth;
         // 初始化运行时参数
         currentStack = 0;
         isActive = false;
@@ -45,20 +46,21 @@ public class Buff_8 : BaseBuff
         base.ActivateBuff();
         if(stackType == 0)
         {
-            GameManager.Instance.data.globalData["initialHealth"] = GameManager.Instance.data.globalData["initialHealth"].ToObject<float>() + value;
+            GameManager.Instance.initialHealth = GameManager.Instance.initialHealth + value;
             GameManager.Instance.currentHealth = GameManager.Instance.currentHealth + value;
         }
         else if(stackType == 1)
         {
-            GameManager.Instance.data.globalData["initialHealth"] = GameManager.Instance.data.globalData["initialHealth"].ToObject<float>() * value;
+            GameManager.Instance.initialHealth = GameManager.Instance.initialHealth * value;
             GameManager.Instance.currentHealth = GameManager.Instance.currentHealth * value;
         }
+        // Debug.Log($"buff_8激活，当前血量：{GameManager.Instance.currentHealth}/{GameManager.Instance.initialHealth},原血量：{originalinitialHealth}/{originalinitialHealth}");
     }
 
     public override void DeactivateBuff()
     {
         base.DeactivateBuff();
-        GameManager.Instance.data.globalData["initialHealth"] = originalinitialHealth;
-        GameManager.Instance.currentHealth = originalcurrentHealth;
+        GameManager.Instance.initialHealth = originalinitialHealth;
+        // GameManager.Instance.currentHealth = originalcurrentHealth;
     }    
 }
