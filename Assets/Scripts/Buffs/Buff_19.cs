6562b6ae1273987a9cce264b9579cf0dea8f39b9
FortunateRui <68178029+FortunateRui@users.noreply.github.com>
2025-07-15 12:07:38 +0800

    buff18命名更正

---
+++ Assets/Scripts/Buffs/Buff_19.cs
@@ -0,0 +1,59 @@
+/*
+文件名：Buff_18.cs
+编辑人：没道理啊
+文件描述：多多清汤buff
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+public class Buff_19 : BaseBuff
+{
+    private float value;
+    private float originalsplitModeCD;
+
+    public override void Init()
+    {
+        // 初始化buff参数
+        buffId = 19;
+        // 在buffs数组中查找对应buffId的配置
+        var buffsArray = GameManager.Instance.data.buffData["buffs"] as JArray;
+        var buffConfig = buffsArray.FirstOrDefault(b => b["buffId"].ToObject<int>() == buffId);
+        buffName = buffConfig["buffName"].ToString();
+        buffDescription = buffConfig["buffDescription"].ToString();
+        buffIcon = buffConfig["buffIcon"].ToString();
+        buffDuration = buffConfig["buffDuration"].ToObject<float>();
+        buffStackable = buffConfig["buffStackable"].ToObject<bool>();
+        stackType = buffConfig["stackType"].ToObject<int>();
+        isNormalBuff = buffConfig["isNormalBuff"].ToObject<bool>();
+        value = buffConfig["value"].ToObject<float>();
+        
+        // 初始化运行时参数
+        currentStack = 0;
+        isActive = false;
+        target = null;
+    }
+
+    public override void ActivateBuff()
+    {
+        // 保留父类逻辑
+        base.ActivateBuff();
+        originalsplitModeCD = GameManager.Instance.data.modeData["splitModeCD"].ToObject<float>();
+        if(stackType == 0)
+        {
+            GameManager.Instance.data.modeData["splitModeCD"] = GameManager.Instance.data.modeData["splitModeCD"].ToObject<float>() + value;
+        }
+        else if(stackType == 1)
+        {
+            GameManager.Instance.data.modeData["splitModeCD"] = GameManager.Instance.data.modeData["splitModeCD"].ToObject<float>() * value;
+        }
+    }
+
+    public override void DeactivateBuff()
+    {
+        base.DeactivateBuff();
+        GameManager.Instance.data.modeData["splitModeCD"] = originalsplitModeCD;
+    }    
+}
