b2a269e4e86db707503c22e64807d1f44729b0d9
3025361190 <119148001+3025361190@users.noreply.github.com>
2025-07-19 22:16:39 +0800

    buff_22

---
+++ Assets/Scripts/Buffs/Buff_22.cs
@@ -0,0 +1,59 @@
+/*
+文件名：Buff_22.cs
+编辑人：没道理啊
+文件描述：稀有buff2
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+public class Buff_22 : BaseBuff
+{
+    private float value;
+    private float originalsplitNum;
+
+    public override void Init()
+    {
+        // 初始化buff参数
+        buffId = 22;
+        // 在buffs数组中查找对应buffId的配置
+        var buffsArray = GameManager.Instance.data.buffData["buffs"] as JArray;
+        var buffConfig = buffsArray.FirstOrDefault(b => b["buffId"].ToObject<int>() == buffId);
+        buffName = buffConfig["buffName"].ToString();
+        buffDescription = buffConfig["buffDescription"].ToString();
+        buffIcon = buffConfig["buffIcon"].ToString();
+        buffDuration = buffConfig["buffDuration"].ToObject<float>();
+        buffStackable = buffConfig["buffStackable"].ToObject<bool>();
+        stackType = buffConfig["stackType"].ToObject<int>();
+        isNormalBuff = buffConfig["isNormalBuff"].ToObject<bool>();
+        value = buffConfig["value"].ToObject<float>();
+        originalsplitNum = GameManager.Instance.data.bulletData["splitNum"].ToObject<float>();
+        // 初始化运行时参数
+        currentStack = 0;
+        isActive = false;
+        target = null;
+    }
+
+    public override void ActivateBuff()
+    {
+        // 保留父类逻辑
+        base.ActivateBuff();
+        
+        if(stackType == 0)
+        {
+            GameManager.Instance.data.bulletData["splitNum"] = GameManager.Instance.data.bulletData["splitNum"].ToObject<float>() + value;
+        }
+        else if(stackType == 1)
+        {
+            GameManager.Instance.data.bulletData["splitNum"] = GameManager.Instance.data.bulletData["splitNum"].ToObject<float>() * value;
+        }
+    }
+
+    public override void DeactivateBuff()
+    {
+        base.DeactivateBuff();
+        GameManager.Instance.data.bulletData["splitNum"] = originalsplitNum;
+    }    
+}
